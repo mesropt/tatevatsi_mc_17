3f3d04a7ee3541662cf731a0b1ff5f5a61eca86e
Mesrop <mesroptnamak@gmail.com>
2023-07-02 16:11:38 +0400

    Started basket, doctors, examinations, users and home apps

---
+++ readme.md
@@ -0,0 +1,12 @@
+# Medical Clinic Website based on Django
+
+Educational project of a medical clinic, which was developed during training at the Armenian Code Academy.
+
+## Stack
+
+* `Python` > 3.11
+* `Django` < 4.2.2
+
+## License
+
+MIT
--- tatevatsi_mc_17/settings.py
+++ tatevatsi_mc_17/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "home.apps.HomeConfig",
+    "examinations.apps.ExaminationsConfig",
+    "users.apps.UsersConfig",
+    "basket.apps.BasketConfig",
+    "doctors.apps.DoctorsConfig",
 ]
 
 MIDDLEWARE = [
