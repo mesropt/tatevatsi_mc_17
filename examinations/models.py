c722546d4cdb89a076994c77a152e828442f65f0
Mesrop <mesroptnamak@gmail.com>
2023-07-04 21:16:56 +0400

    Develop 2. Updated doctors/models.py and some other changes

--- examinations/models.py
+++ examinations/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+# from helpers.file_storage import movie_images_storage, producer_images_storage
+from django.core.exceptions import ValidationError
 
-# Create your models here.
+
+# def validate_rate(value):
+#     if value > 10:
+#         raise ValidationError('Value can not be more than 10')
+#     return value
+
+
+class Examination(models.Model):
+    mri = models.CharField(max_length=50, choices=MRIExaminationChoices.choices)
+    ct = models.CharField(max_length=50, choices=CTExaminationChoices.choices)
+    x_ray = models.CharField(max_length=50, choices=XRAYExaminationChoices.choices)
+    mammography = models.CharField(max_length=50, choices=MammographyExaminationChoices.choices)
+    ultrasound = models.CharField(max_length=50, choices=UltrasoundExaminationChoices.choices)
+    # producer = models.ForeignKey('Producer', on_delete=models.CASCADE)
+    # name = models.CharField(max_length=150)
+    # year = models.IntegerField()
+    # rate = models.FloatField(null=True, blank=True, validators=[validate_rate])
+    # description = models.TextField()
+    # created_at = models.DateTimeField(auto_now_add=True)
+    # updated_at = models.DateTimeField(auto_now=True)
+    # image = models.ImageField(null=True, blank=True, upload_to=movie_images_storage)
+
+    def __str__(self):
+        return self.mri
---
+++ helpers/choices.py
@@ -0,0 +1,33 @@
+from django.db import models
+
+
+class MRIExaminationChoices(models.TextChoices):
+    radiosurgery_stereotaxic_mri = 'MRI after radiosurgery or stereotaxic interventions', 'MRI after radiosurgery or stereotaxic interventions'
+    breast_mri = 'Breast MRI', 'Breast MRI'
+    spine_mri = 'Spine MRI', 'Spine MRI'
+    brain_mri = 'Brain MRI', 'Brain MRI'
+    joints_mri = 'Joints MRI', 'Joints MRI'
+    arteries_veins_mri = 'MRI of arteries and veins', 'MRI of arteries and veins'
+    male_pelvic_mri = 'MRI of male pelvic organs', 'MRI of male pelvic organs'
+    female_pelvic_mri  = 'MRI of female pelvic organs', 'MRI of female pelvic organs'
+    soft_tissues_mri = 'MRI of soft tissues', 'MRI of soft tissues'
+    abdomen_retroperitoneal_mri = 'MRI of abdomen and retroperitoneal space', 'MRI of abdomen and retroperitoneal space'
+
+# class CTExaminationChoices(models.TextChoices):
+#     male = 'M', 'Male'
+#     femail = 'F', 'Female'
+#
+#
+# class XRAYExaminationChoices(models.TextChoices):
+#     male = 'M', 'Male'
+#     femail = 'F', 'Female'
+#
+#
+# class MammographyExaminationChoices(models.TextChoices):
+#     male = 'M', 'Male'
+#     femail = 'F', 'Female'
+#
+#
+# class UltrasoundExaminationChoices(models.TextChoices):
+#     male = 'M', 'Male'
+#     femail = 'F', 'Female'
