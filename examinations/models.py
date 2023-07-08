fe628e7c414c862b79de7d0e9929d99143ea4b08
Mesrop <mesroptnamak@gmail.com>
2023-07-08 08:06:37 +0400

    Разрешение конфликтов

--- examinations/models.py
+++ examinations/models.py
@@ -4,10 +4,6 @@ from helpers.file_storage import examination_images_storage
 
 # ABSTRACT CLASS:
 class Examination(models.Model):
-
-    class Meta:
-        abstract = True
-
     name = models.CharField(max_length=255)
     description = models.TextField(null=True, blank=True)
     doctor = models.ManyToManyField('Doctor', related_name='examinations', null=True, blank=True)
@@ -16,7 +12,10 @@ class Examination(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
     price = models.FloatField(null=True, blank=True)
 
-    # @abstractmethod
+    class Meta:
+        abstract = True
+
+    @abstractmethod
     def __str__(self):
         return self.name
 
@@ -72,3 +71,80 @@ class Ultrasound(Examination):
     class Meta:
         verbose_name = 'Ultrasound'
         verbose_name_plural = 'Ultrasounds'
+
+#
+# # INSTANCES:
+# # MRI instances:auth_user_user_permissions
+# Spine MRI(1.5T) one part = MagneticResonanceImaging()
+# Spine MRI(3T) one part = MagneticResonanceImaging()
+# Brain MRI(1.5T) = MagneticResonanceImaging()
+# Brain MRI(3T) = MagneticResonanceImaging()
+# Breast MRI(1.5T) = MagneticResonanceImaging()
+# Breast MRI(3T) = MagneticResonanceImaging()
+# Joints MRI(1.5T) = MagneticResonanceImaging()
+# Joints MRI(3T) = MagneticResonanceImaging()
+# MRI of abdomen and retroperitoneal space(1.5T) = MagneticResonanceImaging()
+# MRI of abdomen and retroperitoneal space(3T) = MagneticResonanceImaging()
+# MRI of soft tissues (1.5T) = MagneticResonanceImaging()
+# MRI of soft tissues (3T) = MagneticResonanceImaging()
+# MRI of arteries and veins (1.5T) = MagneticResonanceImaging()
+# MRI of arteries and veins (3T) = MagneticResonanceImaging()
+# Contrast_material (1.5T) = MagneticResonanceImaging()
+# Contrast_material (3T) = MagneticResonanceImaging()
+
+
+#
+#
+# # ComputedTomography instances:
+# ct_computer_layering_polyorgan = ComputedTomography()
+# ct_use_of_contrast_material = ComputedTomography()
+# ct_one_system_brain = ComputedTomography()
+# ct_one_system_spine_bones = ComputedTomography()
+# ct_paranasal_sinuses = ComputedTomography()
+# ct_complex_angiography_lower_limb_vessels = ComputedTomography()
+# ct_one_system_chest = ComputedTomography()
+# ct_two_system_chest = ComputedTomography()
+# ct_dentition_upper_lower_jaws = ComputedTomography()
+# ct_abdominal_pelvic_urinary_tract_without_contrast = ComputedTomography()
+# ct_temporal_bones = ComputedTomography()
+# ct_one_system_anthography = ComputedTomography()
+# ct_two_system_anthography = ComputedTomography()
+# ct_three_system_anthography = ComputedTomography()
+# ct_coronary_arteries_angiography = ComputedTomography()
+# ct_aortic_thoracic_abdominal = ComputedTomography()
+# ct_neck_brain_arteries = ComputedTomography()
+# ct_cardiac_angiography = ComputedTomography()
+# ct_additional_organ_system = ComputedTomography()
+#
+# # # XRay
+# Abdominal x-ray
+# Bone x-ray
+# Chest x-ray
+# Dental x-ray
+# Extremity x-ray
+# Joint x-ray
+# Spine x-ray
+# Lungs X-ray
+# Skull X-Ray
+# Hand X-Ray
+# Kidney, Ureter and Bladder X-Ray
+# Neck X-Ray
+# # # name = XRay()
+# # name = XRay()
+# #
+# # # Mammography
+# Mammography
+# # name = Mammography()
+# # name = Mammography()
+#
+# Ultrasound
+#     Heart and blood vessels, including the abdominal aorta and its major branches
+#     Liver
+#     Mammary glands
+#     Brain
+#     spleen
+#     Pelvic organs
+#     Internal organs
+#     kidneys
+#     Abdominal organs
+#     uterus, ovaries, and unborn child (fetus) in pregnant patients
