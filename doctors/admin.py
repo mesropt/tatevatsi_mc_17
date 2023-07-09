031d5704b9c2dc45fd1cca3809e5b1745b9357fc
Mesrop <mesroptnamak@gmail.com>
2023-07-09 18:51:05 +0400

    resolviing conflicts

--- doctors/admin.py
+++ doctors/admin.py
@@ -17,5 +17,4 @@ class DoctorAdmin(admin.ModelAdmin):
                  ('Ultrasound', {'fields': ('ultrasound',)}))
     filter_horizontal = ('magnetic_resonance_imaging_1_5t', 'magnetic_resonance_imaging_3t', 'computed_tomography', 'computed_tomography', 'xray', 'mammography', 'ultrasound')
 
-
 admin.site.register(Doctor, DoctorAdmin)
