e4d2560f50a336a2f998e6948852aca516fe0681
Mesrop Tarkhanyan <74879366+mesropt@users.noreply.github.com>
2023-07-09 18:15:43 +0400

    Develop_s5. Updeted urls, views, htmls (#12)
    Co-authored-by: SamvelMkrtchyan <samvel.mkrtchyan2014@gmail.com>

--- home/views.py
+++ home/views.py
@@ -2,3 +2,11 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'home/index.html')
+
+
+def about(request):
+    return render(request, 'home/about.html')
+
+
+def contact(request):
+    return render(request, 'home/contact.html')
