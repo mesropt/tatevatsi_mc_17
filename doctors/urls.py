826c4ae28ec6286df951a5e3de6ad5f64ccc0199
mesropt <mesroptnamak@gmail.com>
2023-07-09 23:58:30 +0400

    Develop_m8. CBV in doctors/view started

--- doctors/urls.py
+++ doctors/urls.py
@@ -4,7 +4,7 @@ from doctors import views
 app_name = 'doctors'
 urlpatterns = [
     # path('list', views.DoctorListView.as_view(), name='list'),
-    path('list', views.doctors_list, name='list')
+    path('list', views.DoctorsListView.as_view(), name='list')
     # path('details/<int:pk>/', views.DoctorDetailView.as_view(), name='detail'),
     # path('add/', views.DoctorAddView.as_view(), name='add'),
     # path('update/<int:pk>/', views.DoctorUpdateView.as_view(), name='update'),
--- doctors/views.py
+++ doctors/views.py
@@ -1,9 +1,26 @@
 from django.views.generic import TemplateView, DetailView
-from doctors.models import Doctor
-from django.shortcuts import render, get_object_or_404, redirect
 from django.core.paginator import Paginator
+from doctors.models import Doctor
 
+class DoctorsListView(TemplateView):
+    template_name = 'doctors/doctors_list.html'
+    paginate_by = 4
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        doctors = Doctor.objects.all().order_by('-pk')
+        paginator = Paginator(doctors, self.paginate_by)
+        page_number = self.request.GET.get("page")
+        doctors = paginator.get_page(page_number)
+        context['doctors_list'] = doctors
+        return context
+
+# def doctors_list(request):
+#     doctors = Doctor.objects.all().order_by('-pk')
+#     paginator = Paginator(doctors, 4)  # Show 25 contacts per page.
+#     page_number = request.GET.get("page")
+#     doctors = paginator.get_page(page_number)
+#     return render(request, 'doctors/doctors_list.html', {'doctors_list': doctors})
 
 # class DoctorDetailView(DetailView):
 #     model = Doctor
@@ -23,17 +40,3 @@ from django.core.paginator import Paginator
 # class DoctorDeleteView(DetailView):
 #     model = Doctor
 #     template_name = 'examination/detail.html'
-
-
-# class DoctorListView(DetailView):
-#     model = Doctor
-#     template_name = 'doctors/doctors_list.html'
-#
-
-
-def doctors_list(request):
-    doctors = Doctor.objects.all().order_by('-pk')
-    paginator = Paginator(doctors, 4)  # Show 25 contacts per page.
-    page_number = request.GET.get("page")
-    doctors = paginator.get_page(page_number)
-    return render(request, 'doctors/doctors_list.html', {'doctors_list': doctors})
