bc12cd883ffc840a826395d514c9b5a4b1b13ba0
Mesrop Tarkhanyan <74879366+mesropt@users.noreply.github.com>
2023-07-15 14:34:53 +0400

    Develop_m21c. Updatd users delete (#37)

--- authnapp/urls.py
+++ authnapp/urls.py
@@ -10,4 +10,8 @@ urlpatterns = [
     path("register/", authnapp.register, name="register"),
     path("edit/", authnapp.edit, name="edit"),
     path('profile/<int:pk>/', authnapp.UserDetailView.as_view(), name='profile'),
+    path('profile/delete/<int:pk>/', authnapp.delete_user, name='user_delete'),
+    path('profile/update/<int:pk>/', authnapp.UserUpdateView.as_view(),
+         name='user_update'),
+
 ]
