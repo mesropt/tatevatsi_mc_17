f7c751c854f6eb2a0b5f4a09835e6e3de5dbb49c
Mesrop Tarkhanyan <74879366+mesropt@users.noreply.github.com>
2023-07-15 17:42:55 +0400

    Develop_m23. Small changes (#40)

--- authnapp/urls.py
+++ authnapp/urls.py
@@ -8,7 +8,6 @@ urlpatterns = [
     path("login/", authnapp.login, name="login"),
     path("logout/", authnapp.logout, name="logout"),
     path("register/", authnapp.register, name="register"),
-    path("edit/", authnapp.edit, name="edit"),
     path('profile/<int:pk>/', authnapp.UserDetailView.as_view(), name='profile'),
     path('profile/delete/<int:pk>/', authnapp.delete_user, name='user_delete'),
     path('profile/update/<int:pk>/', authnapp.UserUpdateView.as_view(),
